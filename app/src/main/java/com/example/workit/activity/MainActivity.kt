6fae1fd89586d4e95e89a2976de136ebe4d29831
JustinRudat <justin.rudat@etu.upmc.fr>
2019-02-20 09:15:29 +0100

    project creation

---
+++ app/src/main/java/com/example/workit/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.workit.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.workit.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
